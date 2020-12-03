3a9609a582cd08f2d6a0ab35139d075aa2fd10b7
nigg <thuse91@live.se>
2020-12-03 01:10:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function oss(){
+    document.querySelector(".row1").style.display = "none"
+    document.querySelector(".row2").style.display = "block"
+    document.querySelector("#btn1").style.display = "none"
+    document.querySelector("#btn2").style.display = "block"
+}
+function prob(){
+    document.querySelector(".row2").style.display = "none"
+    document.querySelector(".row1").style.display = "block"
+    document.querySelector("#btn1").style.display = "block"
+    document.querySelector("#btn2").style.display = "none"
+}
